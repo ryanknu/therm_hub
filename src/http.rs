b7a4f85182a3732d8c5ccebb183db035c169b759
Ryan Knuesel <ryan@mypaynet.com>
2020-08-20 12:18:57 -0500

    Order of operations is hard

--- src/http.rs
+++ src/http.rs
@@ -36,10 +36,10 @@ pub async fn start() {
     let server = server.serve(make_service_fn(|_connection| async {
         Ok::<_, Infallible>(service_fn(|req: Request<Body>| async move {
             println!("[ hyper] Incoming: {}", req.uri().path());
-            let path = if !is_authorized(&req) {
-                &"/forbidden"
-            } else if req.method().eq(&Method::OPTIONS) {
+            let path = if req.method().eq(&Method::OPTIONS) {
                 &"/options"
+            } else if !is_authorized(&req) {
+                &"/forbidden"
             } else {
                 req.uri().path()
             };
